452c69b0d90fe2a5c2cc1f32d8b5747ed347790b
Thomas Klausner <domm@cpan.org>
2008-10-22 12:00:05 +0200

    some more db helpers

---
+++ helper/import_dpd_data.pl
@@ -0,0 +1,18 @@
+#!/opt/perl5.10/bin/perl
+
+use strict;
+use warnings;
+use 5.010;
+
+use Business::DPD::DBIC;
+
+my $sourcedir = $ARGV[0] || 'data';
+die "sourcedir missing or does not exist!" unless -d $sourcedir;
+
+Business::DPD::DBIC->import_data({source=>$sourcedir});
+
+__END__
+
+=head1 NAME
+
+
