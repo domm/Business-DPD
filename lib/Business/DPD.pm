d967f9c63cdeb1f0c2ec29bb3616a5bcff48e953
Maros Kollar <maros@k-1.com>
2012-09-05 15:20:02 +0200

    Version 0.21

--- lib/Business/DPD.pm
+++ lib/Business/DPD.pm
@@ -4,7 +4,7 @@ use strict;
 use warnings;
 use 5.010;
 
-use version; our $VERSION = version->new('0.20');
+use version; our $VERSION = version->new('0.21');
 
 use parent qw(Class::Accessor::Fast);
 use Business::DPD::DBIC;
